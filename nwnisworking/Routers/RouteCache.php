67c94963b54c620e5095053f34337ba8cd2fa117
nwnisworking <66354122+nwnisworking@users.noreply.github.com>
2026-04-15 04:11:48 +0000

    Add RouteCache class for managing and rebuilding route cache

---
+++ nwnisworking/Routers/RouteCache.php
@@ -0,0 +1,111 @@
+<?php
+namespace nwnisworking\Routers;
+
+use function count;
+
+use nwnisworking\App;
+use nwnisworking\Bootable;
+use nwnisworking\Logger;
+use ReflectionClass;
+
+final class RouteCache implements Bootable{
+  private const string CACHE_FILE = 'cache/routes.php';
+
+  private const string CONTROLLER_FILES = 'nwnisworking/Controllers/*.php';
+
+  private static bool $booted = false;
+
+  public static function boot(App $app) : void{
+    Logger::log('Booting route cache...');
+
+    $cache = self::CACHE_FILE;
+    $controllers = glob(self::CONTROLLER_FILES);
+
+    if(self::isCacheValid($cache, $controllers)){
+      Logger::log('Route cache loaded');
+      $data = require_once $cache;
+
+      self::$booted = true;
+      return;
+    }
+    else{
+      Logger::log('Route cache is invalid, rebuilding...');
+    }
+
+    $routes = self::buildCache($controllers);
+
+    Logger::log('Route cache built with ' . count($routes) . ' routes');
+    file_put_contents($cache, '<?php return ' . var_export($routes, true) . ';');
+  }
+
+  private static function isCacheValid(string $cache, array $controllers) : bool{
+    if(!file_exists($cache)){
+      return false;
+    }
+
+    $cacheTime = filemtime($cache);
+
+    foreach($controllers as $controller){
+      if(filemtime($controller) > $cacheTime){
+        return false;
+      }
+    }
+
+    return true;
+  }
+
+  private static function buildCache(array $controllers) : array{
+    $routes = [];
+
+    Logger::log('Building route cache...');
+
+    foreach($controllers as $controller){
+      $controller = str_replace(['.php', '/'], ['', '\\'], $controller);
+      $reflection = new ReflectionClass($controller);
+
+      // Can't build abstract class or interface
+      if($reflection->isAbstract() || $reflection->isInterface()){
+        continue;
+      }
+
+      Logger::log("Processing controller: $controller");
+
+      $classAttribute = $reflection->getAttributes(Route::class)[0] ?? null;
+
+      if($classAttribute){
+        $classRoute = $classAttribute->newInstance();
+      }
+
+      foreach($reflection->getMethods() as $method){
+        $methodAttributes = $method->getAttributes(Route::class);
+
+        foreach($methodAttributes as $methodAttribute){
+          $childRoute = $methodAttribute->newInstance();
+          $combineRoute = $classRoute ? $classRoute->combine($childRoute) : $childRoute;
+
+          foreach($combineRoute->methods as $httpMethod){
+            $httpMethod = strtoupper($httpMethod);
+
+            Logger::log("Generating route: $httpMethod $combineRoute->path with middlewares: " . implode(', ', $combineRoute->middlewares));
+
+             $fullPath = '/' . trim($combineRoute->path, '/');
+             $middlewares = $combineRoute->middlewares;
+
+            $routes["$httpMethod $fullPath"] = [
+              'controller' => $controller,
+              'method' => $method->getName(),
+              'path' => $fullPath,
+              'middlewares' => $middlewares
+            ];
+          }
+        }
+      }
+    }
+
+    return $routes;
+  }
+
+  public static function isBooted() : bool{
+    return self::$booted;
+  }
+}
