62fcb67defdeb4ba32d7a2d3b8941af65c1aa3e6
nwnisworking <66354122+nwnisworking@users.noreply.github.com>
2026-04-15 05:49:46 +0000

    Refactor index.php to update method names and add View singleton

--- index.php
+++ index.php
@@ -1,9 +1,12 @@
 <?php
+
+use nwnisworking\View;
 spl_autoload_register(fn($class) => require_once str_replace('\\', DIRECTORY_SEPARATOR, $class) . '.php');
 
 use nwnisworking\App;
 use nwnisworking\Routers\RouteCache;
 
 $app = App::getInstance();
-$app->boot(RouteCache::class);
+$app->bootable(RouteCache::class);
+$app->singleton('view', View::class);
 $app->run();
