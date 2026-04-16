c206b9ac3de26a98202ec3b08294a541f218e0ac
nwnisworking <66354122+nwnisworking@users.noreply.github.com>
2026-04-16 11:04:44 +0000

    Refactor AppController for testing purposes.

--- nwnisworking/Controllers/AppController.php
+++ nwnisworking/Controllers/AppController.php
@@ -1,20 +1,38 @@
 <?php
 namespace nwnisworking\Controllers;
 
+use nwnisworking\HTTP\Request;
+use nwnisworking\HTTP\Session;
 use nwnisworking\View;
 use nwnisworking\Routers\Route;
 
-#[Route('/', middlewares: ['app'])]
+#[Route('/')]
 final class AppController{
-  public function __construct(private View $view){}
+  public function __construct(private View $view, private Session $session){}
 
-  #[Route('', ['GET'], middlewares : ['auth'])]
-  public function index() : string{
-    return $this->view->render('index', ['title' => 'Muse - Peer to Peer Audio Sharing']);
+  #[Route('', ['GET'])]
+  public function index(Request $request) : string{
+    $user = $this->session->get('user');
+
+    return $this->view->render('index', [
+      'title' => 'Muse - Peer to Peer Audio Sharing', 
+      'auth' => $user,
+      'content' => $this->view->render($user ? 'dashboard' : 'welcome')
+    ]);
   }
 
-  #[Route('/signin', ['GET', 'POST'])]
-  public function signin() : string{}
+  #[Route('/sign-in', ['GET', 'POST'], ['auth'])]
+  public function signin() : string{
+    $user = $this->session->get('user');
+    var_dump($this->session);
+    // return $this->view->render('index', [
+    //   'title' => 'Muse | Sign In',
+    //   'auth' => $user,
+    //   'content' => $this->view->render()
+    // ]);
+
+    return '4';
+  }
 
   #[Route('/signup')]
   public function signup() : string{}
