6fcd3f3b3eab2decd2485bdfc70d4b228b06e74b
nwnisworking <nwnisworking@gmail.com>
2026-07-28 01:45:25 +0800

    fk

--- nwnisworking/Middlewares/Middleware.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace nwnisworking\Middlewares;
-
-use nwnisworking\HTTP\Request;
-use nwnisworking\HTTP\Response;
-
-interface Middleware{
-  public function handle(Request $request, Response $response, callable $next) : string;
-}
